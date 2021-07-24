454056f147a880d0e82cf66c55f6b7a57b43a9b9
Diego Buenrostro <dbuenrostro64@csu.fullerton.edu>
2021-07-24 16:57:53 -0700

    fixed ghost button presses issue

--- src/main.cpp
+++ src/main.cpp
@@ -18,16 +18,22 @@ unsigned long currentMillisSpin;
 const unsigned long SPIN_PERIOD = 3000;
 unsigned long startMillisServo;
 unsigned long currentMillisServo;
-const unsigned long SERVO_PERIOD = 500;
+const unsigned long SERVO_PERIOD = 1500;
 String spinDirection = "cw";
-int servoAngle = 65;
+int servoAngle = 125;
 int spinSpeed = 50;
 int buttonState = 0;
 volatile byte buttonMode = LOW;
 
+int lastButtonState = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+unsigned long timeHeld = 0;
+int holdButtonTime = 3000;
+
 char report[80];
 
-//Servo laserServo;
+Servo laserServo;
 Adafruit_LIS3MDL magnet;
 LSM6 gyroAccel;
 
@@ -50,68 +56,78 @@ void setup()
   pinMode(MOTOR_PWM_2, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), mode, FALLING); // run mode routine whenever pin goes high to low
   digitalWrite(LASER_PIN, LOW);
-  digitalWrite(RED_LED_PIN, HIGH); // red LED on as long as device is on
-  //laserServo.attach(SERVO_PIN);
+  digitalWrite(GREEN_LED_PIN, HIGH); // red LED on as long as device is on
+  laserServo.attach(SERVO_PIN);
 
   //-----IMU initialize-----
 
   // magnetometer
-  if (! magnet.begin_I2C()) 
-  {          // hardware I2C mode, can pass in address & alt Wire
-    Serial.println("Failed to find LIS3MDL chip");
-    while (1) { delay(10); }
-  }
-  magnet.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  magnet.setOperationMode(LIS3MDL_CONTINUOUSMODE);
-  magnet.setDataRate(LIS3MDL_DATARATE_155_HZ);
-  magnet.setRange(LIS3MDL_RANGE_4_GAUSS);
-  magnet.setIntThreshold(500);
-  magnet.configInterrupt(false, false, true, // enable z axis
-                          true, // polarity
-                          false, // don't latch
-                          true); // enabled!
+  // if (! magnet.begin_I2C()) 
+  // {          // hardware I2C mode, can pass in address & alt Wire
+  //   Serial.println("Failed to find LIS3MDL chip");
+  //   while (1) { delay(10); }
+  // }
+  // magnet.setPerformanceMode(LIS3MDL_MEDIUMMODE);
+  // magnet.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+  // magnet.setDataRate(LIS3MDL_DATARATE_155_HZ);
+  // magnet.setRange(LIS3MDL_RANGE_4_GAUSS);
+  // magnet.setIntThreshold(500);
+  // magnet.configInterrupt(false, false, true, // enable z axis
+  //                         true, // polarity
+  //                         false, // don't latch
+  //                         true); // enabled!
+
   //gyroscope and accelerometer
-  if (!gyroAccel.init())
-  {
-    Serial.println("Failed to detect and initialize IMU!");
-    while (1);
-  }
-  gyroAccel.enableDefault();
-  
-  
+  // if (!gyroAccel.init())
+  // {
+  //   Serial.println("Failed to detect and initialize IMU!");
+  //   while (1);
+  // }
+  // gyroAccel.enableDefault();
+
 }
 
 void loop() 
 {
   spinDirection = pressButton();
   buttonDirectionChange(spinDirection);
+  autoServoChange(SERVO_PERIOD);
 }
 
 String pressButton(void)
 {
-  String spinDirection;
+  static String spinDirection = "a";
+  static bool buttonPressed = 0;
   buttonState = digitalRead(BUTTON_PIN);
-  if(buttonMode == HIGH)
+  if ((millis() - lastDebounceTime) > debounceDelay)
   {
-    spinDirection = "cw";
+    if(buttonState == LOW && buttonPressed == 0)
+    {
+      if (spinDirection == "a")
+      {spinDirection = "b";}
+      else if (spinDirection == "b")
+      {spinDirection = "a";}
+      buttonPressed = 1;
+      lastDebounceTime = millis();
+    }
+    if(buttonState == HIGH)
+      {
+        buttonPressed = 0;
+        lastDebounceTime = millis();
+      }
   }
-  else if(buttonMode == LOW)
-  {
-    spinDirection = "ccw";
-  }
+  //Serial.print(spinDirection);
   return spinDirection;
 }
 
-
 void buttonDirectionChange(String spinDirection)
 {
-  if(spinDirection == "cw")
+  if(spinDirection == "a")
   {
-    Serial.println("hello");
     analogWrite(MOTOR_PWM_1, spinSpeed);
     analogWrite(MOTOR_PWM_2, 0);
   }
-  else if(spinDirection == "ccw")
+  else if(spinDirection == "b")
   {
     analogWrite(MOTOR_PWM_1, 0);
     analogWrite(MOTOR_PWM_2, spinSpeed);
@@ -142,21 +158,21 @@ void autoDirectionChange(int interval)
 
 void autoServoChange(int interval)
 {
-  // currentMillisServo = millis();
-  // if(currentMillisServo - startMillisServo >= interval)
-  // {
-  //   if (servoAngle==65)
-  //   {
-  //     servoAngle=45;
-  //     laserServo.write(servoAngle);
-  //   }
-  //   else if (servoAngle==45)
-  //   {
-  //     servoAngle=65;
-  //     laserServo.write(servoAngle);
-  //   }
-  //   startMillisServo = currentMillisServo;
-  // }
+  currentMillisServo = millis();
+  if(currentMillisServo - startMillisServo >= interval)
+  {
+    if (servoAngle==125)
+    {
+      servoAngle=95;
+      laserServo.write(servoAngle);
+    }
+    else if (servoAngle==95)
+    {
+      servoAngle=125;
+      laserServo.write(servoAngle);
+    }
+    startMillisServo = currentMillisServo;
+  }
 }
 
 void readMagnetometer(void)
